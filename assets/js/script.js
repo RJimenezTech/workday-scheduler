f904d666ff2e87fae4c04596828a3ab7bdf69a1a
Robert Jimenez <robertjimenez86@gmail.com>
2022-03-30 17:56:38 -0500

    add text input on description area

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,24 +7,25 @@ $("#currentDay").text(today);
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours
 // array that holds the hour blockes of the day that will by displayed
-let hoursArray = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
+// let hoursArray = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
 let hoursObjArray = [
-    {hour:"9AM",description:""},
-    {hour:"10AM",description:""},
-    {hour:"11AM",description:""},
-    {hour:"12PM",description:""},
-    {hour:"1PM",description:""},
-    {hour:"2PM",description:""},
-    {hour:"3PM",description:""},
-    {hour:"4PM",description:""},
-    {hour:"5PM",description:""}];
+    {hour:"9AM",description:"free hour"},
+    {hour:"10AM",description:"free hour"},
+    {hour:"11AM",description:"free hour"},
+    {hour:"12PM",description:"free hour"},
+    {hour:"1PM",description:"free hour"},
+    {hour:"2PM",description:"free hour"},
+    {hour:"3PM",description:"free hour"},
+    {hour:"4PM",description:"free hour"},
+    {hour:"5PM",description:"free hour"}];
 console.log(hoursObjArray)
 // find container div and add "div" with "row" class
 // for as many hours in the day, add as many "row" divs with "col" divs for sections
-for (let i = 0; i < hoursArray.length; i++){
-    let thisHourNumOnly = hoursArray[i].slice(0,-1).slice(0,-1).trim();
-    $(".container").append("<div class='row' data-hour='" + thisHourNumOnly + "'><div class='col-1'></div><div class='col-1 hour'>" + hoursArray[i] + "</div><div class='col-7 bg-dark description'></div><div class='col-1 saveBtn'></div><div class='col-1'></div></div>");
+for (let i = 0; i < hoursObjArray.length; i++){
+    let thisHourNumOnly = hoursObjArray[i].hour.slice(0,-1).slice(0,-1).trim();
+    $(".container").append("<div class='row' data-hour='" + thisHourNumOnly + "'><div class='col-1'></div><div class='col-1 hour'>" + hoursObjArray[i].hour + "</div><div class='col-7 bg-dark description'>" + hoursObjArray[i].description +"</div><div class='col-1 saveBtn'></div><div class='col-1'></div></div>");
 };
+
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
 // function that checks each rows time and compares it with the current hour
@@ -57,7 +58,6 @@ let auditTime = function(el) {
     }
 };
 // initiate audit once upon loading website
-// find a row each row and audit time
 $(".row").each(function(el) {
     auditTime($(this));
 });
@@ -67,13 +67,25 @@ setInterval(function() {
     $(".row").each(function(el) {
         auditTime($(this));
     })
-}, 5000)
+}, (1000 * 60) * 60)
 
 // WHEN I click into a time block
 // THEN I can enter an event
-
-
+let inputText = function(el) {
+    let textInput = $("<textarea>").addClass("col-7 description textarea").text("");
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+}
 // WHEN I click the save button for that time block
 // THEN the text for that event is saved in local storage
+let saveInput = function() {
+    
+}
+
 // WHEN I refresh the page
 // THEN the saved events persist
+
+// waiting for click on a description area
+$(".description").on("click",inputText);
+// waiting for click on save button
+$(".saveBtn").on("click",saveInput);
